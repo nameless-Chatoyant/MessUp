86cb9c3ed897b851e24a0a4a94f0b8b0e23164bc
JesseYang <jesse.yang1985@gmail.com>
2017-10-29 15:26:35 +0800

    .

--- MessUp/operations/scale.py
+++ MessUp/operations/scale.py
@@ -12,12 +12,12 @@ class Resize(Operation):
         if self.dsize:
             d_h, d_w = self.dsize
             if d_h is not None and d_w is not None:
-                print('before:', img.shape)
+                # print('before:', img.shape)
                 if 'interpolation' in self.__dict__ and  self.interpolation == 'nearest':
                     res = cv2.resize(img, (d_w, d_h), interpolation=cv2.INTER_NEAREST)
                 else:
                     res = cv2.resize(img, (d_w, d_h))
-                print('after:', img.shape)
+                # print('after:', img.shape)
             elif d_h is None and d_w is not None:
                 scale = d_w/w
                 if 'interpolation' in self.__dict__ and self.interpolation == 'nearest':
